f9e4f92966ac4d861587dc35783bc3eedb3aaebc
duff9 <danetheridge@msn.com>
2023-08-14 10:38:12 +0100

    add custom display for models

--- forecasting_system/model.py
+++ forecasting_system/model.py
@@ -9,12 +9,26 @@ class Model:
         self.model_class = model_class
         self.configuration = configuration
 
-    def __train(self, observation_data):
+    def train(self, observation_data):
         return
 
     def predict(self, forecast_data):
-        return forecast_data
+        return
 
     def reset(self):
         self.trained_model = None
         self.observation_data = None
+        self.is_trained = False
+
+    def __repr__(self):
+        # Custom display when using print on a model
+        return "Model({}, {})".format(self.model_class, self.configuration)
+
+    def __str__(self):
+        # Custom display when using print on a model
+        if self.trained_model is None:
+            print_text = 'Model '
+        else:
+            print_text = 'Trained Model '
+
+        return print_text + "({}, {})".format(self.model_class, self.configuration)
