d4c437622575ca036b705cf8a979309d727b31d3
duff9 <danetheridge@msn.com>
2023-08-23 12:20:30 +0100

    add pacf plot and update energy example

--- forecasting_system/data_analyser.py
+++ forecasting_system/data_analyser.py
@@ -18,5 +18,18 @@ def plot_correlation_heatmap(dataframe):
 
 
 def plot_autocorrelation(series, lags=10):
+    """The correlation between two values in the same time series"""
     tsaplots.plot_acf(series, lags=lags, title="Autocorrelation for: " + series.name)
     plt.show()
+
+
+def plot_partial_autocorrelation(series, lags=10):
+    """The correlation between two values excluding the correlations accounted for at smaller lags
+    """
+    tsaplots.plot_pacf(series, lags=lags, title="Partial autocorrelation for: " + series.name)
+    plt.show()
+
+
+def calculate_principle_component_analysis():
+    # TODO
+    return
