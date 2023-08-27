f2f50407fa14f11ae75e8e395809359d869aa856
duff9 <danetheridge@msn.com>
2023-08-27 13:51:21 +0100

    update test data

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -4,4 +4,4 @@ import forecasting_system.data_loader as dl
 def test_read_data_file():
     data = dl.read_data_file('test_data.csv')
     assert (not data.empty)
-    assert (len(data['Observation']) == 3)
+    assert (len(data['Observation']) == 4)
